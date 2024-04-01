13cfeeaa74e13ef2b495534942a48ab295f28516
Rohan Singh <rohan.singh@ufl.edu>
2024-04-01 16:30:42 -0400

    message1

--- main.py
+++
@@ -1,6 +0,0 @@
-from Website import create_app
-
-app = create_app()
-
-if __name__ == '__main__':  # only runs website if main.py is used
-    app.run(debug=True)  # everytime a change is made in python code, it will automatically rerun the server
